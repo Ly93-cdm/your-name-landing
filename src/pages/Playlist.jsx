b62eda2c21c7ed05b79abacfcdc1aa6756263ec6
Ly <noorasaetre371@gmail.com>
2025-08-19 16:31:27 -0300

    add song

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,19 +1,164 @@
 import React from "react";
 import './Playlist.css';
 
+const albumCover = "https://i.scdn.co/image/ab67616d0000b2733d1869d8c477d291a205a2d6";
+
 const tracks = [
-    // adicionar as musicas
   {
-    title: "Zenzenzense",
+    title: "Dream lantern",
     artist: "RADWIMPS",
-    link: "https://open.spotify.com/track/xyz",
-    cover: "https://i.scdn.co/image/ab67616d0000b273e3e5f3e5a6e5f3e5e3e5f3e5"
+    link: "https://open.spotify.com/intl-pt/track/5mEqD00bdFcsiVd0MfvEeF?si=61683d0f34a848a7",
+    cover: albumCover
   },
   {
-    title: "Sparkle",
+    title: "School road",
     artist: "RADWIMPS",
-    link: "https://open.spotify.com/track/abc",
-    cover: "https://i.scdn.co/image/ab67616d0000b273abcabcabcabcabcabcabcabc"
+    link: "https://open.spotify.com/intl-pt/track/0Hi7B2lRZ2wNjmnDjPjh3D?si=3261fefa75d94f55",
+    cover: albumCover
+  },
+  {
+    title: "Itomori high school",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/5OczsuojyL2tzu2MkdKmDu?si=9cd3fa4419b6444a",
+    cover: albumCover
+  },
+  {
+    title: "First view of tokyo",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/5hIWiDRrmApdkw8P7Kuzjz?si=cceb73bcfe49496e",
+    cover: albumCover
+  },
+  {
+    title: "Cafe at last",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/19JvFc7PGpwKs9cIq0Aes5?si=993d7546a26343d8",
+    cover: albumCover
+  },
+  {
+    title: "Theme of Ms. Okudera",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/1hapjpP0xlw5zfw4QqanqD?si=ea12c748cd814a6a",
+    cover: albumCover
+  },
+  {
+    title: "Unusual changes of two",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/23wbvIPlLBO8Ef3kceOCYO?si=2825f27125ef4ab1",
+    cover: albumCover
+  },
+  {
+    title: "Zenzenzense - movie ver.",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/2DLrgv7HhJanCuD8L9uJLR?si=0615be8c06774986",
+    cover: albumCover
+  },
+  {
+    title: "Goshintai",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/1QOnVBRGEtt3JaM7ZnLBxb?si=731f843fd96d457c",
+    cover: albumCover
+  },
+  {
+    title: "Date",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/7A7lxMWZI2Uow7WqWjWGcM?si=167345a5bcf841b9",
+    cover: albumCover
+  },
+  {
+    title: "Autumn festival",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/1BikQ3e5AUfDSdzTosW9bE?si=818eb7cc4580414c",
+    cover: albumCover
+  },
+  {
+    title: "Evoking memories",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/4c3vF1y4ggD8WdMB2gNjZL?si=40bc945de9354e3e",
+    cover: albumCover
+  },
+  {
+    title: "Visit to Hida",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/3zM5JfQf6e7EfrjaBGRvy5?si=f0fe938e2b1a4068",
+    cover: albumCover
+  },
+  {
+    title: "Disappeared town",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/7n5lU2pd2KXzkMQSWJCmn9?si=7d475e7a203542b9",
+    cover: albumCover
+  },
+  {
+    title: "Library",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/23mLwgHatcxmvxQqRYcjHo?si=faf998423a5d41ef",
+    cover: albumCover
+  },
+  {
+    title: "The night inn",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/5ukAh8hcLesj7pNuBmTjeC?si=7f161e0daeff4efe",
+    cover: albumCover
+  },
+  {
+    title: "Again to Goshitai",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/5axz8UbmqJO9Yr0w8eMKUe?si=a663111e066e42f2",
+    cover: albumCover
+  },
+  {
+    title: "Kuchikamizake trip",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/22gL8bOImYbV8QscJq9v2p?si=5bf21293da21437d",
+    cover: albumCover
+  },
+  {
+    title: "Council of war",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/208TvaHtChWPChoQxSvrhJ?si=8181ed21857d47f4",
+    cover: albumCover
+  },
+  {
+    title: "Persuading mayor",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/0FXM7rvkGhau2nFzF5f2OV?si=5dbb87b83ef840a0",
+    cover: albumCover
+  },
+  {
+    title: "Thema of Mitsuha",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/66HNv8qd9jr7q5P9nNTOOZ?si=f32a5e045d164860",
+    cover: albumCover
+  },
+  {
+    title: "Unseen two",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/0smg4CHGXE5Xkc9oCmfHQq?si=3c9b066ae80d48a5",
+    cover: albumCover
+  },
+  {
+    title: "Katawaredoki",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/4WedBZTeFawYCBCgfj36iK?si=2100d6a8da8e480b",
+    cover: albumCover
+  },
+  {
+    title: "Sparkle - movie ver.",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/3A4FRzgve9BjfKbvVXRIFO?si=4b031128c3fe41a5",
+    cover: albumCover
+  },
+  {
+    title: "Date 2",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/0wf56xkFyoiPBPDhqgksMt?si=9c330a96bcc94fde",
+    cover: albumCover
+  },
+  {
+    title: "Nandemonaiya - movie edit.",
+    artist: "RADWIMPS",
+    link: "https://open.spotify.com/intl-pt/track/6RM5WJcShIHsxrja11M1iK?si=056943d505444e81",
+    cover: albumCover
   }
 ];
 
